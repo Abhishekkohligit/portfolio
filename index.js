0a957284bc394525d0991f32866c5863c235204e
Abhishek Kohli <ab02110211@gmail.com>
2023-11-28 21:53:24 +0000

    autocheck

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ toggleSwitch.addEventListener("click", () => {
 // type effect
 
 var typingEffect = new Typed(".typedText", {
-	strings: ["Developer", "Data Analyst", "Poker Dealer", "Dad"],
+	strings: ["Web Developer", "Data Analyst", "Poker Dealer", "Dad"],
 
 	loop: true,
 	typeSpeed: 100,
